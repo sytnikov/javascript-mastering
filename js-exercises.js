a82fcac4eb0968120755b29c0ddeb491438a863a
sytnikov <sytnikov.alexey@gmail.com>
2023-05-03 23:06:28 +0300

    added first function

--- js-exercises.js
+++ js-exercises.js
@@ -1,2 +1,9 @@
 console.log('Hello, World!');
 console.log('- Did Joffrey agree?\n- He did. He also said "I love using \\n".');
+
+// First function: prints the text
+const printMotto = () => {
+    const text = 'Winter is coming';
+    console.log(text);
+  }
+  
